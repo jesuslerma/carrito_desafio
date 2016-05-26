262fd6b7f7e5ea310e34c636c613d777df766cf9
jesus lerma sanchez <shuyojl@gmail.com>
2016-05-25 21:35:14 -0500

    add changes just with carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :last_cart, ->{ order 'created_at desc'}, class_name: 'Cart'
+  has_many :carts
+  after_create :build_cart
+
+  def build_cart
+    new_cart = carts.build(total_price:0, open:true)
+    new_cart.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :carts
-  resources :products
+  resources :products do
+    resources :orders, only: :create
+  end
   root 'products#index'
 end
